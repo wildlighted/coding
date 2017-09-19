87cdf222ef7c63928c35ee31655ffa65b2852d36
Студент НИУ ВШЭ <student@basm-402-std03.staff.corp.local>
2017-09-19 11:53:48 +0300

    first class

---
+++ code1.py
@@ -0,0 +1,16 @@
+#print('O gospodi')
+#print('x =', x, '; y =', y)
+#state = 'a'
+#print(state*6 + '!'*2)
+#num_str = '3.5'
+#num = float(num_str)
+#print(num)
+#name = input("What's your name?")
+#print('Hi', name)
+#print(type(name))
+x = float(input('Enter 2 numbers:'))
+y = float(input())
+#print(x*y)
+if x >=3 :
+    print('Про второе число я ничего не знаю')
+    print('А первое число не меньше трех')
