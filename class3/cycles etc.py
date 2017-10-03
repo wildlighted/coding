dddaa70458a37a1ca2e3dc6a55db91f560a8c58b
Студент НИУ ВШЭ <student@basm-401-std06.staff.corp.local>
2017-10-03 11:50:07 +0300

    циклы и прочее

---
+++ class3/cycles etc.py
@@ -0,0 +1,84 @@
+word = 'prisms'
+# print(word)
+
+a = 0
+# for letter in word:
+ #   print(letter)
+ #   print(letter*8)
+
+#for i in word:
+ #   print(i, a)
+  #  a+=1
+
+#print(a)
+#print('='*10)
+# print(word[0])
+# print(len(word))
+# print(word[len(word)-1])
+# print(word[-4])
+# индексы считаются с нуля; элементы строки можно нумеровать
+
+# for i in word:
+#    a +=1
+#    if a % 2 != 0:
+#        print(i)
+
+#print(word[1:4]) #не включительно
+#print(word[:5])
+#print(word[3:])
+#print(word[::3])
+#print(word[:-2])
+#print(word[-5:-1])
+#print(word[::-2])
+#part = word[3:6]
+#print(part)
+
+ans = 0
+#for i in range(3, 20, 2):
+  #  ans += i
+  #  print(i, ans)
+    #print()
+    # print('i =', i)
+    #if i > 10 and i % 2 != 0:
+    #    print(i**2, )
+
+# x = int(input('Enter your number:'))
+#for i in range (x+1):
+ #   ans += i
+ #   print(i, ans)
+#print(ans)
+
+#i = 2
+#while i**2 <= 100:
+ #   print(i, i**2)
+ #   i += 1
+#print('i =', i)
+
+#for i in range(10):
+ #   print(i, end=', ')
+    #if i**2 >= 37:
+     #   break
+  #  if i == 8:
+   #     continue #переходит к следующей итерации, не заходя в эту
+    #print(i**2)
+
+#for i in range(5):
+#    word = input('Enter your word: ')
+#    print(len(word))
+
+# x = int(input())
+#for i in range(x):
+#    y = int(input('Enter your number:'))
+#    if y <= 0:
+ #       print('below zero')
+ #   elif y >= 0:
+ #       print('above zero')
+ #   else:
+  #      print('zero')
+
+z = len(str(32738))
+print(z)
+
+print('End.')
+        
+
