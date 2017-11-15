79987a92dd025732cce8ba468c27cae425e7d4e3
wildlighted <31654732+wildlighted@users.noreply.github.com>
2017-11-15 19:34:43 +0300

    Update hw3.py

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,5 +1,13 @@
 #Вариант 4
 word = input('Введите слово: ')
-for i in range(len(word)):
-    print(word)
-    word = word[-1] + word[:-1]
+if word == '':
+   word = input('Нет, введите непустую строку в качестве слова: ')
+if word != '':
+    for i in range(len(word)):
+        print(word)
+        word = word[-1] + word[:-1]
+else:
+    print('Ну, видимо, вы не хотите ничего вводить. Что тут поделаешь.')
+  
+        
+    
