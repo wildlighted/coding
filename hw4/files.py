3825d497116810bbab3765c7ac45232897661383
wildlighted <31654732+wildlighted@users.noreply.github.com>
2017-11-22 11:54:15 +0300

    Update files.py

--- hw4/files.py
+++ hw4/files.py
@@ -2,15 +2,24 @@ with open('text.txt', encoding='utf-8') as f:
     text = f.readlines()
 k = 0
 lentext = 0
-for line in text:
-    line = line.strip('\n')
-    words = line.split()
-    lentext += len(words)
-    for word in words:
-        if not word.endswith('.') and not word.endswith(','):
-            k += 1
-percentage = k/lentext*100
-answer = 'В этом файле ' + str(percentage) + '% слов не оканчиваются точкой или запятой.'
+if not text:
+    answer = 'Упс, файл пуст.'
+else:
+    for line in text:
+        line = line.strip('\n')
+        if line:
+            words = line.split()
+            if words:
+                lentext += len(words)
+                for word in words:
+                    if not word.endswith('.') and not word.endswith(','):
+                        k += 1
+                percentage = k / lentext * 100
+                answer = 'В этом файле ' + str(percentage) + '% слов не оканчиваются точкой или запятой.'
+            else:
+                answer = 'Упс, в файле только пробелы или переносы строки.'
+        else:
+            answer = 'Упс, в файле только пробелы или переносы строки.'
 print(answer)
 
 #вариант 4
