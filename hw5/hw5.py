2c50931b3de7a2ca3e45062bef199c95c7ceeaf2
wildlighted <31654732+wildlighted@users.noreply.github.com>
2017-11-29 23:33:40 +0300

    Update hw5.py

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -5,7 +5,11 @@ if word == '':
     print('Вы ничего не ввели.')
 else:
     while word != '':
-        if word.endswith('are') or word.endswith('ere') or word.endswith('ire') or word.endswith('ari') or word.endswith('eri') or word.endswith('iri'):
+        # вариант на случай, если предполагается, что пользователь может вводить рандомные сочетания латинских букв:
+        # if word.endswith('are') or word.endswith('ere') or word.endswith('ire') 
+        # or word.endswith('ari') or word.endswith('eri') or word.endswith('iri'):
+        # вариант на случай, если пользователь может вводить только реальные латинские слова:
+        if word.endswith('re') or word.endswith('ri'):
             inf.append(word + '\n')
         word = input()
 with open('text.txt', 'w', encoding='utf-8') as f:
