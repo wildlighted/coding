a66d6eb198ea26f85f5094e4688ee3ce53ea07df
wildlighted <31654732+wildlighted@users.noreply.github.com>
2017-11-29 11:26:27 +0300

    Add files via upload

---
+++ hw5.py
@@ -0,0 +1,12 @@
+# вариант 4цщквы
+inf = []
+word = input('Введите латинское слово: ')
+if word == '':
+    print('Вы ничего не ввели.')
+else:
+    while word != '':
+        if word.endswith('are') or word.endswith('ere') or word.endswith('ire') or word.endswith('ari') or word.endswith('eri') or word.endswith('iri'):
+            inf.append(word + '\n')
+        word = input()
+with open('text.txt', 'w', encoding='utf-8') as f:
+    f.writelines(inf)
