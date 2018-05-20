eb619b30ac9650d52aa1e8c32380e86907fe3c49
wildlighted <31654732+wildlighted@users.noreply.github.com>
2018-05-20 17:35:02 +0300

    Add files via upload

---
+++ hw13/hw13.py
@@ -0,0 +1,19 @@
+#вариант 4
+import os
+
+def countnames():
+    filenames = os.listdir()
+    symcount = {}
+    for filename in filenames:
+        if filename[0].lower() in symcount.keys():
+            symcount[filename[0].lower()] += 1
+        else:
+            symcount[filename[0].lower()] = 0
+    return symcount
+
+def main():
+    letters_counted = countnames()
+    print(sorted(letters_counted, key=letters_counted.get, reverse=True)[0])
+
+if __name__ == '__main__':
+    main()
